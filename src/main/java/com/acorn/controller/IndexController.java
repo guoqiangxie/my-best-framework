ac5be32eeb7b157b0a648c94910e0c4de27b8046
xieguoqiang <xieguoqiang@chinadrtv.com>
2013-07-22 18:05:13 +0800

    add spring support

---
+++ src/main/java/com/acorn/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.acorn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Title: IndexController
+ * Description:
+ * User: xieguoqiang
+ *
+ * @version 1.0
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("index")
+    public String index() {
+        System.out.println("hello");
+        return "index/first";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index/first.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xieguoqiang
+  Date: 13-7-22
+  Time: 下午4:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>my first page</title>
+</head>
+<body>
+
+</body>
+</html>
