38d872fc5ae6ebf637eb95b26d940240713486a6
xieguoqiang <xieguoqiang@chinadrtv.com>
2013-07-23 13:11:58 +0800

    add spring support

--- src/main/java/com/acorn/controller/IndexController.java
+++ src/main/java/com/acorn/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.acorn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
